02e24909b3cfad4cc6577b6860752ffc7204e4d8
StevenAnchundia <erick.anchundia01@cu.ucsg.edu.ec>
2026-06-14 13:13:54 -0500

    clase mascota

--- programacion_poo/mascota.py
+++ programacion_poo/mascota.py
@@ -1,1 +1,22 @@
+class Mascota:
 
+    def __init__(self, nombre, especie, edad):
+        self.nombre = nombre
+        self.especie = especie
+        self.edad = edad
+
+    def mostrar_informacion(self):
+        print("----- INFORMACIÓN DE LA MASCOTA -----")
+        print(f"Nombre : {self.nombre}")
+        print(f"Especie: {self.especie}")
+        print(f"Edad   : {self.edad} años")
+
+    def hacer_sonido(self):
+        if self.especie.lower() == "perro":
+            print(f"{self.nombre} dice: ¡Guau Guau!")
+        elif self.especie.lower() == "gato":
+            print(f"{self.nombre} dice: ¡Miau!")
+        elif self.especie.lower() == "loro":
+            print(f"{self.nombre} dice: ¡Hola!")
+        else:
+            print(f"{self.nombre} hace un sonido.")
