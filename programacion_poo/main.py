0924004722f5f3907a42f52612e34352b50e38f1
StevenAnchundia <erick.anchundia01@cu.ucsg.edu.ec>
2026-06-14 13:13:30 -0500

    Main mascotas

--- programacion_poo/main.py
+++ programacion_poo/main.py
@@ -1,1 +1,11 @@
+from mascota import Mascota
 
+mascota1 = Mascota("Broly", "Perro", 4)
+mascota2 = Mascota("Felipe", "Gato", 2)
+
+mascota1.mostrar_informacion()
+mascota1.hacer_sonido()
+
+print()
+mascota2.mostrar_informacion()
+mascota2.hacer_sonido()
