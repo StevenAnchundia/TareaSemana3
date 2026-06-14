f8144f6ddfa112eb633f2dd8096daa75bccaf763
StevenAnchundia <erick.anchundia01@cu.ucsg.edu.ec>
2026-06-14 13:11:13 -0500

    Add mascota forma treadicional

--- programacion_tradicional/tradicional.py
+++ programacion_tradicional/tradicional.py
@@ -1,1 +1,15 @@
+def registrar_mascota():
+    nombre = input("Ingrese el nombre de la mascota: ")
+    especie = input("Ingrese la especie: ")
+    edad = int(input("Ingrese la edad: "))
 
+    return nombre, especie, edad
+
+def mostrar_mascota(nombre, especie, edad):
+    print("\n----- INFORMACIÓN DE LA MASCOTA -----")
+    print(f"Nombre : {nombre}")
+    print(f"Especie: {especie}")
+    print(f"Edad   : {edad} años")
+
+nombre, especie, edad = registrar_mascota()
+mostrar_mascota(nombre, especie, edad)
